c10d46a84f00871278b5c16a3dcf59ece2c4806f
VitoriaSant <svitoria9815@gmail.com>
2023-12-21 15:50:23 -0300

    Telas de cadastro

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-// importando o express
 const express = require("express");
 const app = express();
 
-//informando que será utilizado o ejs para criar o front
+//Definindo que vou utilizar o ejs para fazer o html
 app.set('view engine','ejs');
+//Definindo que minha aplicação vai usar arquivos estaticos (CSS, imagens...)
+app.use(express.static('public'));
 
-//caminhos 
-app.get("/",(req, res) => {res.render("index");});
+app.get ("/",(req,res) => {
+    res.render("index");
+});
 
-//iniciando a aplicação na porta 8080
-app.listen(8080,() => {console.log("Rodando...");});
+app.get ("/CadEmpresa",(req,res) => {
+    res.render("CadEmpresa");
+});
+
+app.get ("/CadEquipamento",(req,res) => {
+    res.render("CadEquipamento");
+});
+
+app.get ("/CadSituacao",(req,res) => {
+    res.render("CadSituacao");
+});
+
+app.post ("/salvarpergunta",(res,req) => {
+    res.send("Pergunta recebida");
+});
+
+app.listen(443,()=>{ console.log("Show");}); 
+
